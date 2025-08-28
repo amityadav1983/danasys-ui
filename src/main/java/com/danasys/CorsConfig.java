19e226dc1ddda854252fcd4118ce7922ec5b1667
amityadav1983 <yadav.1983@gmail.com>
2025-08-28 23:56:41 +0530

    removing cors
    removing cors

--- src/main/java/com/danasys/CorsConfig.java
+++ src/main/java/com/danasys/CorsConfig.java
@@ -1,18 +1,15 @@
 package com.danasys;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//@Configuration
+public class CorsConfig //implements //WebMvcConfigurer 
+{
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
+  /*  @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // adjust if needed
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-    }
+    }*/
 }
