f527d3c12ee2a708f0db5e977a321130923dc401
amityadav1983 <yadav.1983@gmail.com>
2025-10-05 21:18:51 +0530

    My Order history
    My Order history

--- src/main/java/com/danasys/OrderAndPaymentController.java
+++ src/main/java/com/danasys/OrderAndPaymentController.java
@@ -183,6 +183,87 @@ public class OrderAndPaymentController {
 		
 		return ResponseEntity.badRequest().body(null);
 	}
+	
+	@GetMapping("/api/order/myOrderHistory/{userProfileId}")
+	public ResponseEntity<List<OrderDetailsDTO>> myOrderHistory(@PathVariable Long userProfileId) {
+		
+		if(userProfileId>0) {
+		List<OrderDetailsDTO> orderList = new ArrayList<>();
+
+		OrderDetailsDTO order = new OrderDetailsDTO();
+		order.setId(1l);
+		order.setDeliveryAddress("Dummy Address");
+		order.setOrderDeliverTimeSlot(new Date());
+		order.setOrderStatus(OrdarStatusEnum.ORDER_PLACED);
+		order.setTotalDiscount(20.20d);
+		order.setTotalPrice(100.12d);
+
+		OrderDetailsDTO order1 = new OrderDetailsDTO();
+		order1.setId(2l);
+		order1.setDeliveryAddress("Dummy Address");
+		order1.setOrderDeliverTimeSlot(new Date());
+		order1.setOrderStatus(OrdarStatusEnum.ORDER_PLACED);
+		order1.setTotalDiscount(20.20d);
+		order1.setTotalPrice(100.12d);
+
+		PurchasedProductDTO product = new PurchasedProductDTO();
+		product.setMRP(200d);
+		product.setOfferPrice(120d);
+		product.setQuantity(2);
+		product.setStatus(OrdarStatusEnum.ORDER_PLACED);
+		product.setReturnDateLimit(new Date());
+		product.setPurchasedProductId(1l);
+
+		PurchasedProductDTO product1 = new PurchasedProductDTO();
+		product1.setMRP(400d);
+		product1.setOfferPrice(220d);
+		product1.setQuantity(4);
+		product1.setStatus(OrdarStatusEnum.ORDER_PLACED);
+		product1.setReturnDateLimit(new Date());
+		product1.setPurchasedProductId(2l);
+
+		List<PurchasedProductDTO> productList = new ArrayList<>();
+		productList.add(product);
+		productList.add(product1);
+
+		order.setProducts(productList);
+		order1.setProducts(productList);
+
+		OrderTrackerDTO trackerP = new OrderTrackerDTO();
+		;
+
+		OrderTrackerDTO trackerC = new OrderTrackerDTO();
+		trackerC.setRole(UserRoleEnum.ROLE_USER);
+		trackerC.setActionOn(new Date());
+		trackerC.setActor("USER 1");
+		trackerC.setAction(OrdarStatusEnum.IN_PROGRESS.name());
+		trackerP.getTracker().add(trackerC);
+
+		OrderTrackerDTO trackerC1 = new OrderTrackerDTO();
+		trackerC1.setRole(UserRoleEnum.ROLE_USER);
+		trackerC1.setActionOn(new Date());
+		trackerC1.setActor("USER 1");
+		trackerC1.setAction(OrdarStatusEnum.ORDER_PLACED.name());
+		trackerP.getTracker().add(trackerC1);
+
+		OrderTrackerDTO trackerC2 = new OrderTrackerDTO();
+		trackerC2.setRole(UserRoleEnum.ROLE_BUSINESS_USER);
+		trackerC2.setActionOn(new Date());
+		trackerC2.setActor("Gupta Ji");
+		trackerC2.setAction(OrdarStatusEnum.DELIVERED.name());
+		trackerP.getTracker().add(trackerC2);
+
+		order.setOrderTracker(trackerP);
+		order1.setOrderTracker(trackerP);
+
+		orderList.add(order);
+		orderList.add(order1);
+
+		return ResponseEntity.ok(orderList);
+		}
+		
+		return ResponseEntity.badRequest().body(null);
+	}
 
 	@PostMapping("/api/order/updateOrderStatusByBU")
 	public ResponseEntity<String> updateOrderStatus(@RequestBody OrderStatusUpdateRequest orderStatusUpdateRequest) {
