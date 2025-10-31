e1f30eceb20c6ac557de44556b96d04b2ffbba50
amityadav1983 <yadav.1983@gmail.com>
2025-10-31 14:13:40 +0530

    request param send profile id

--- src/main/java/com/danasys/OrderAndPaymentController.java
+++ src/main/java/com/danasys/OrderAndPaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -620,7 +621,7 @@ public class OrderAndPaymentController {
 	}
 
 	@PostMapping("/api/order/redeemReferalPoint")
-	public ResponseEntity<String> redeemReferalPoint(Long userProfileId) {
+	public ResponseEntity<String> redeemReferalPoint(@RequestParam Long userProfileId) {
 		return ResponseEntity.ok("Point Redeemed ! ");
 
 	}
